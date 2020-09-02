e007e9b997b733578491da7370be6cef3293201f
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-09-02 10:00:09 +0300

    Add new tests

--- tests/tr00_pdf/ts01_recursive/t01_recursive.sh
+++ tests/tr00_pdf/ts01_recursive/t01_recursive.sh
@@ -2,17 +2,17 @@
 
 source ../../../lib/asserts.sh
 
-NAME=no_params
+NAME=recursive
 
 EXP=$(cat -e exp)
-RES="$(./$1 | cat -e)"
+RES="$(./$1 -R | cat -e)"
 ASSERT_EQUAL "$EXP" "$RES" -h
 
-OUT_DIR="out_t00"
+OUT_DIR="out_t01"
 mkdir -p $OUT_DIR
 echo "$EXP" > $OUT_DIR/exp
 echo "$RES" > $OUT_DIR/res
-diff $OUT_DIR/exp $OUT_DIR/res > "$OUT_DIR/diff.txt"
+diff $OUT_DIR/exp $OUT_DIR/res > "$OUT_DIR/diff"
 
 run_leaks_check $1 $NAME
 
