91d666c6a504186bcbc14f1e878191d02ac79ed6
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-09-07 22:36:05 +0300

    Minor changes

--- tests/tr00_pdf/ts01_recursive/t01_recursive.sh
+++ tests/tr00_pdf/ts01_recursive/t01_recursive.sh
@@ -12,7 +12,7 @@ OUT_DIR="out_t01"
 mkdir -p $OUT_DIR
 echo "$EXP" > $OUT_DIR/exp
 echo "$RES" > $OUT_DIR/res
-diff $OUT_DIR/exp $OUT_DIR/res > "$OUT_DIR/diff"
+diff $OUT_DIR/exp $OUT_DIR/res > "$OUT_DIR/diff.txt"
 
 run_leaks_check $1 $NAME
 
